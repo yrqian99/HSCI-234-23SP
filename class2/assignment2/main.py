919e1b4c94a3940da0f4ae57bfe474cd5d77b031
yrqian99 <109845506+yrqian99@users.noreply.github.com>
2023-02-04 14:57:26 -0800

    Delete class2/assignment2 directory

--- class2/assignment2/main.py
+++
@@ -1,9 +0,0 @@
-import js
-p5 = js.window
-
-def setup():
-    p5.createCanvas(300, 300)    # 300 x 300 pixel canvas 
-
-def draw():
-    p5.fill(255,10,120)
-    p5.background(20)           # white background
